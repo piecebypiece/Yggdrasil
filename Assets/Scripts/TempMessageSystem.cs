37819ec3abbd379595a80a7d2dce781994f3718e
Jiny <jiny9789@naver.com>
2022-01-10 16:01:58 +0900

    옵저버 패턴 수정ver 0.1

--- Assets/Scripts/TempMessageSystem.cs
+++ Assets/Scripts/TempMessageSystem.cs
@@ -17,8 +17,23 @@ namespace TempCodes
 		}
 	}
 
-	//옵저버 추상클래스  == 이벤트 추상 클래스
-	//: 옵저버들이 구현해야 할 인터페이스 메서드  == 이벤트에 구현할 공통적인 매서드
+	public class MoveEvent : IEvent
+	{
+		public void Dispose()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+
+
+	public class temp
+	{
+
+
+	}
+
+
+
 	public interface IEventHandler
 	{
 		public abstract void OnNotify(IEvent e);
@@ -41,9 +56,10 @@ namespace TempCodes
 	{
 		public void OnNotify(IEvent e)
 		{
-			Debug.Log("이벤트2 호출");
+			Debug.Log($"{e.ToString()} 이벤트2 호출");
 		}
 	}
+
 
 
 	//대상클래스 == 직접적으로 이벤트를 호출할 클래스
@@ -59,13 +75,33 @@ namespace TempCodes
 		//약속(인터페이스) 목록  == 반드시 구현해야 되는 것
 		public void Subscribe<T>(IEventHandler e)
 		{
-			EventListenerDic.Add(typeof(T), new List<IEventHandler>());
-			EventListenerDic[typeof(T)].Add(e);
+			//EventListenerDic.Add(typeof(T),  new List<IEventHandler>());  // 구독할때마다 새로운 리스트가 계속 생겨남
+			//EventListenerDic[typeof(T)].Add(e);
+
+
+			//타입이 있는지 찾아보고 없으면 새로운 리스트를 생성해서 추가.
+			if (EventListenerDic.ContainsKey(typeof(T)))
+			{
+				EventListenerDic[typeof(T)].Add(e);
+
+
+			}
+			else
+			{
+				//eventList.Add((TempCodes.IEvent)typeof(T));  //구독할때 이벤트리스트에 추가해주기
+				EventListenerDic.Add(typeof(T), new List<IEventHandler>());
+				EventListenerDic[typeof(T)].Add(e);
+			}
+
+
 		}
 		public void Unsubscribe<T>(IEventHandler e)
 		{
 
-			if (EventListenerDic[typeof(T)].Contains(e)) EventListenerDic[typeof(T)].Remove(e);
+			if (EventListenerDic[typeof(T)].Contains(e)) EventListenerDic[typeof(T)].Remove(e);  //사전에서도 지워준다.
+
+
+
 		}
 		public void Notify()
 		{
@@ -75,34 +111,60 @@ namespace TempCodes
 			//	EventList[i].Notify();
 			//}
 
+			//디버깅
+			Debug.Log(eventList.Count);
+
 			//foreach 이용
 			foreach (var e in eventList)
 			{
+
 				var listners = EventListenerDic[e.GetType()];
 
 				foreach (var listner in listners)
 				{
+
 					listner.OnNotify(e);
 				}
 			}
-			eventList.Clear();
+
+			//eventList.Clear();
+
+			//foreach(var e in eventList)
+			//{
+			//	e.Dispose();//Dispose 사용
+			//}
+
+
 		}
 
 		// Start is called before the first frame update
 		void Start()
 		{
+
+			eventList.Add(new MoveEvent());
+			eventList.Add(new DamageEvent());
+
 			IEventHandler eve1 = new EventHandler1();
 			IEventHandler eve2 = new EventHandler2();
 
 
-			//Subscribe<temp>(eve1);
-			//Subscribe<temp>(eve2);
+
+
+			Subscribe<MoveEvent>(eve1);
+			Subscribe<DamageEvent>(eve2);
+
 
 			Notify();
 
-			//Unsubscribe<temp>(eve2);
+			Unsubscribe<DamageEvent>(eve2);
 
 			Notify();
+
+
+
+			eventList.Clear();
+
+
 		}
 
 		// Update is called once per frame
